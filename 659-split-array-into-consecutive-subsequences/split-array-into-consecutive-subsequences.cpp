ad7b617b8c57b9fb9beb4798bb7ba295a3141cff
komalkvy <komal969360@gmail.com>
2025-07-14 15:38:54 +0530

    Time: 13 ms (77.40%) | Memory: 62.1 MB (54.49%) - LeetSync

---
+++ 659-split-array-into-consecutive-subsequences/split-array-into-consecutive-subsequences.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+        unordered_map<int , int> left, seq;
+        int n = nums.size();
+        for(int i = 0; i < n; ++i) ++left[nums[i]];
+        for(int i = 0; i < n; ++i){
+            int x = nums[i];
+            if(!left[x]) continue;
+            if(seq[x - 1]){
+                --left[x];
+                --seq[x - 1];
+                ++seq[x];
+            }
+            else{
+                if(!left[x + 2] || !left[x + 1]) return false;
+                --left[x];
+                --left[x + 1];
+                --left[x + 2];
+                ++seq[x + 2];
+            }
+        }
+        return true;
+    }
+};
