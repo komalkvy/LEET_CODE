8d385af3b748a2ae99cf2da8a2f0f551993188eb
komalkvy <komal969360@gmail.com>
2025-07-24 23:06:03 +0530

    Time: 3 ms (9.11%) | Memory: 15.3 MB (18.00%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -1,43 +1,30 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> result;
-        if (!root) return result;
+        vector<vector<int>> res;
+        if(!root) return res;
 
         queue<TreeNode*> q;
         q.push(root);
-        bool leftToRight = true;
+        bool flag= true; // L to R
 
-        while (!q.empty()) {
-            int size = q.size();
-            vector<int> level(size);
+        while(!q.empty()){
+            int size= q.size();
+            vector<int> row(size);
+            for(int i=0;i<size;i++){
+                TreeNode* node = q.front();
+                q.pop();
 
-            for (int i = 0; i < size; ++i) {
-                TreeNode* node = q.front(); q.pop();
+                int index= flag ? i : (size-1-i);
+                row[index]=node->val;
 
-                // Determine index based on direction
-                int index = leftToRight ? i : size - 1 - i;
-                level[index] = node->val;
-
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
             }
-
-            result.push_back(level);
-            leftToRight = !leftToRight;  // Flip direction
+            flag=!flag;
+            res.push_back(row);
         }
 
-        return result;
+        return res;
     }
 };
