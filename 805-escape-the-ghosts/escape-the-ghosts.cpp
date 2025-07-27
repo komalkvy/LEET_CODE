d2959a11ac85cf0ff90c862fa31b53b5d80fb737
komalkvy <komal969360@gmail.com>
2025-07-27 11:34:31 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (26.97%) - LeetSync

---
+++ 805-escape-the-ghosts/escape-the-ghosts.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool escapeGhosts(vector<vector<int>>& ghosts, vector<int>& target) {
+        int dist = abs(target[0]) + abs(target[1]);
+        for(int i = 0, n = ghosts.size(); i < n; ++i){
+            if(abs(target[0] - ghosts[i][0]) + abs(target[1] - ghosts[i][1]) <= dist) return false;
+        }
+        return true;
+    }
+};
