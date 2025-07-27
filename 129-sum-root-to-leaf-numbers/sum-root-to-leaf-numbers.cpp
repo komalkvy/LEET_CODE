a7e867341eb363bbc4b17d27add3516f4e11cb11
komalkvy <komal969360@gmail.com>
2025-07-27 12:00:42 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (71.55%) - LeetSync

---
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        return dfs(root, 0);
+    }
+
+    int dfs(TreeNode* node, int path) {
+        if (!node) return 0;
+        path = path * 10 + node->val;
+        if (!node->left && !node->right) return path;
+        return dfs(node->left, path) + dfs(node->right, path);
+    }
+};
